3d6ccee2e7f710eea8454e637ae888ddcf4027a4
Pyasma <pranyasharma55555@gmail.com>
2025-12-18 22:40:05 +0530

    Update Gemini API key and fix header image path

--- components/HeaderFile.jsx
+++ components/HeaderFile.jsx
@@ -1,8 +1,12 @@
 export default function HeaderBox() {
-    return (
-        <div className="Box">
-            <img src="../images/chef.jpg" alt="chef cap" className="chef-cap" />
-            <span className="head-text">Chef Gemini</span>
-        </div>
-    )
+  return (
+    <div className="Box">
+      <img
+        src="../public/images/chef.jpg"
+        alt="chef cap"
+        className="chef-cap"
+      />
+      <span className="head-text">Chef Gemini</span>
+    </div>
+  );
 }
