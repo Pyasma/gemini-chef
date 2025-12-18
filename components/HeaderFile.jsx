c916b2b303292d1e55795ccb862fcc5e0ed82763
Pyasma <pranyasharma55555@gmail.com>
2025-12-18 23:18:53 +0530

    Update header image path

--- components/HeaderFile.jsx
+++ components/HeaderFile.jsx
@@ -1,11 +1,7 @@
 export default function HeaderBox() {
   return (
     <div className="Box">
-      <img
-        src="../public/images/chef.jpg"
-        alt="chef cap"
-        className="chef-cap"
-      />
+      <img src="../images/chef.jpg" alt="chef cap" className="chef-cap" />
       <span className="head-text">Chef Gemini</span>
     </div>
   );
